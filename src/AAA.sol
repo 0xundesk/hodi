959f84db4c4057f3b52db5b38b522c05259c1cd0
AAAratings <283016779+AAAratings@users.noreply.github.com>
2026-06-27 13:38:29 +0000

    shell: pragma + interface stub

--- src/AAA.sol
+++ src/AAA.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+interface IAggregator {
+    function latestRound() external view returns (uint256);
+    function getRoundData(uint80 roundId)
+        external
+        view
+        returns (uint80, int256 answer, uint256 startedAt, uint256 updatedAt, uint80);
+}
